0fa5e11141c43ae0b4145fc328d01d2f1efa168b
Anubhav-c9 <anubhavajmera25@gmail.com>
2021-09-30 14:20:03 +0000

    just added these basic files

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ console.log(firstName + " " + lastName);
 const number = 0.3;
 console.log(number);
 
+//primitive data types
+let name1 = 'Anubhav'; // string literal
+let age = 30;          // number literal
+let isApproved = false; //boolean literal
+let secondName = undefined;
+let selectedColor = null;
+
+console.log(name1, age, isApproved, secondName, selectedColor);
+
+console.log(typeof(name1), typeof(age), typeof(isApproved), typeof(secondName), typeof(selectedColor))
