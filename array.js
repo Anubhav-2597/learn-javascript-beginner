1f122510502c81ddb4b07bf491cdfda290b400de
Anubhav-c9 <anubhavajmera25@gmail.com>
2021-09-30 14:18:28 +0000

    learnt about the basic concepts of array

---
+++ array.js
@@ -0,0 +1,16 @@
+//let selectedColors = [];
+
+let selectedColors = ["red","blue"];
+console.log(selectedColors);
+console.log(selectedColors[1]);
+
+selectedColors[2] = "green";
+
+console.log(selectedColors);
+
+selectedColors[3] = 1;
+
+console.log(selectedColors);
+
+//length of the array
+console.log(selectedColors.length);
