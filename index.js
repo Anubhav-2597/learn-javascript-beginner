a17319a3720ecf6b8218ff6c083c92e0cfe84ee8
Anubhav-c9 <anubhavajmera25@gmail.com>
2021-09-30 12:52:43 +0000

    learnt about variables and constants

---
+++ index.js
@@ -0,0 +1,14 @@
+//this is my first javascript code
+// variable
+let name = 'anubhav';
+console.log(name);
+
+let firstName = 'Anubhav';
+let lastName = 'Ajmera';
+
+console.log(firstName + " " + lastName);
+
+//constant 
+const number = 0.3;
+console.log(number);
+
