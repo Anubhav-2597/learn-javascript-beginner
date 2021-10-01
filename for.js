3671b5fc8b857743397dda4ea8c96255732b61c5
Anubhav-c9 <anubhavajmera25@gmail.com>
2021-10-01 14:27:18 +0000

    learnt about different loops

---
+++ for.js
@@ -0,0 +1,18 @@
+for(let i=0; i<5; i++){
+    console.log('Hello World',i);
+}
+
+//while loop
+let j=0;
+while(j<=5){
+    if(j%2 !== 0) console.log(j);
+    j++;
+}
+
+//do while loop
+
+let i = 9;
+do{
+    if ( i%2 !== 0) console.log(i);
+    i++;
+} while (i <=5);
