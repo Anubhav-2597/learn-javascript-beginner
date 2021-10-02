c3dcfca3cdaf0262d960e080ce74a38941ca4779
Anubhav-c9 <anubhavajmera25@gmail.com>
2021-10-02 07:10:29 +0000

    changes done in the code to make it more cleaner

--- demerit-points.js
+++ demerit-points.js
@@ -9,14 +9,14 @@ function checkSpeed(speed){
     const speedLimit = 70;
     const kmPerPoint = 5;
 
-    if(speed < speedLimit + kmPerPoint) 
+    if(speed < speedLimit + kmPerPoint){
         console.log('OK');
-
-    else{
-        const points = Math.floor((speed-speedLimit)/kmPerPoint);
-        if(points>=12) 
-            console.log('License suspended');
-        else 
-            console.log('points',points);
+        return;
     }
+
+    const points = Math.floor((speed-speedLimit)/kmPerPoint);
+    if(points>=12) 
+        console.log('License suspended');
+    else 
+        console.log('points',points);
 }
