84956026c05ab5536b658196f5184ae3f51a6f20
Anubhav-c9 <anubhavajmera25@gmail.com>
2021-10-04 14:38:52 +0000

    code the problem of blog-post-object

---
+++ blog-post-obj.js
@@ -0,0 +1,13 @@
+const blogPost = {
+    title:'Harry Potter',
+    body:'abc',
+    author:'jk rollings',
+    views: 10,
+    comments: [
+        { author:'a', body:'b' },
+        { author:'c', body:'d' },
+    ],
+    isLive: true
+};
+
+console.log(blogPost);
